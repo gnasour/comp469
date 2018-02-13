414c82a5215547429bd558db09be2d08d5d79bf3
George Nassour <gnasour@gmail.com>
2018-02-12 19:47:17 -0800

    added my name to project 2

--- project2/listcomp3.py
+++ project2/listcomp3.py
@@ -1,3 +1,9 @@
+"""
+George Nassour
+AI Comp 469
+Steven Wirsz
+T-TH 2:00-3:15
+"""
 def swap_case_and_print_c(list_of_strings):
     for string in list_of_strings:
         string = string.swapcase()
--- project2/totalCost.py
+++ project2/totalCost.py
@@ -1,12 +1,8 @@
 """
-To run this script, type
-
-  python totalCost.py
-
-Once you have correctly implemented the totalCost function,
-the script should produce the output:
-
-Cost of [('apples', 2.0), ('pears', 3.0), ('limes', 4.0)] is 12.25
+George Nassour
+AI Comp 469
+Steven Wirsz
+T-TH 2:00-3:15
 """
 
 fruitPrices = {'apples':2.00, 'oranges': 1.50, 'pears': 1.75,
